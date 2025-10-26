d65078c4f28241595162bef038db8c9d0b1ce096
Josh Gallant <jgallant34@outlook.com>
2025-10-26 13:55:48 -0300

    fixing links

--- js/main.js
+++ js/main.js
@@ -4,19 +4,19 @@ const games = [
     title: "Snake",
     img: "images/Snake.png",
     desc: "Classic Snake game. Eat the food and grow your snake!",
-    link: "games/Snake.html"
+    link: "games/snake.html"
   },
   {
     title: "Pong",
     img: "images/Pong.png",
     desc: "Retro Pong. Challenge yourself or a friend!",
-    link: "games/Pong.html"
+    link: "games/pong.html"
   },
   {
     title: "Breakout",
     img: "images/Breakout.png",
     desc: "Break bricks and reach the highest score!",
-    link: "games/Breakout.html"
+    link: "games/breakout.html"
   }
 ];
 
